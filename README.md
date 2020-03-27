0a2ba2551cc449910e57ae4d0bb3e01f39e8366d
flb <f.lb@gmx.com>
2020-03-27 23:56:23 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # JC-color-swapper
-A Nintendo Switch homebrew app to change the color of the Joy-Con controllers
+A Nintendo Switch homebrew app to change the color of the Joy-Con controllers. Comes bundled with default themes but you can easily add your own.
+
+## Install
+Copy the `JC-color-swapper` directory to `sdmc:/switch`
+
+## Add custom themes
+You can add custom themes by editing `profiles.json`. You can add new entries and modify existing one, just make sure that you're using the exact same formatting.
+
+
+## Screenshots
+
+![main](https://user-images.githubusercontent.com/61667930/77806502-30ce2900-7085-11ea-9d1f-e495723c79c8.jpg)
+![done](https://user-images.githubusercontent.com/61667930/77806503-3297ec80-7085-11ea-8bd5-41b6b93bf1f8.jpg)
+
---
+++ source/main.cpp
@@ -0,0 +1,143 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include "json.hpp"
+
+
+#include <switch.h>
+
+using json = nlohmann::json;
+
+#define VERSION "1.0"
+
+
+int hexToRGB(std::string hex){
+    return std::stoi(hex, 0, 16);
+}
+
+int setColor(std::vector<int> colors){
+
+    Result pads, ljc, rjc;
+    int res = 0;
+    s32 nbEntries;
+    u64 UniquePadIds[2] = {};
+
+    pads = hidsysGetUniquePadsFromNpad(CONTROLLER_HANDHELD, UniquePadIds, 2,&nbEntries);
+    if(R_SUCCEEDED(pads)){
+        ljc = hiddbgUpdateControllerColor(colors[0], colors[1], UniquePadIds[0]);
+        if (R_FAILED(ljc)) res +=1;
+        rjc = hiddbgUpdateControllerColor(colors[2], colors[3], UniquePadIds[1]);
+        if (R_FAILED(rjc)) res +=2;
+    }
+    else{
+        res +=4;
+    }
+    return res;
+}
+
+int main(int argc, char* argv[])
+{
+    //TODO Create Init function
+    consoleInit(NULL);
+    hiddbgInitialize();
+	hidsysInitialize();
+
+    int currentItem = 0;
+
+    std::ifstream profilesFile("sdmc:/switch/JC/profiles.json");
+    json profilesJson;
+
+    if (!profilesFile.fail()) {
+        try {
+            profilesFile >> profilesJson;
+        } catch(json::parse_error& e) {}
+    }
+
+    std::vector<std::string> colorProfiles;
+    std::vector<std::vector<int>> colorValues;
+
+    for (const auto& x : profilesJson.items()){
+        colorProfiles.push_back(x.value()["name"]);
+        colorValues.push_back({
+            hexToRGB(std::string(x.value()["L_JC"])),
+            hexToRGB(std::string(x.value()["L_BTN"])),
+            hexToRGB(std::string(x.value()["R_JC"])),
+            hexToRGB(std::string(x.value()["R_BTN"]))
+        });
+    }
+
+    int nbItems = colorProfiles.size();
+    if (nbItems > 33) nbItems = 33;
+
+    int currentScreen = 0;
+    int res = 0;
+
+    while (appletMainLoop())
+    {
+        consoleClear();
+        hidScanInput();
+        u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+
+
+        if(currentScreen == 0){
+
+            std::cout << "*** Joy-Con color swapper v" << VERSION << " - By HamletDuFromage" << std::endl;
+            std::cout << "* Select your desired profile with [UP]/[DOWN], and confirm with [A]. \n* Press [B] to quit\n\n" << std::endl;
+
+            res = 0; 
+
+            for (size_t i = 0; i < (size_t) nbItems; i++){
+                if ((int) i == currentItem) std::cout << "> " << colorProfiles[i] << std::endl;
+                else std::cout << colorProfiles[i] << std::endl;
+            }
+
+            //if (kDown & KEY_DOWN){currentItem = std::max(currentItem + 1, nbItems - 1);}
+            if ((kDown & KEY_DOWN) && currentItem < nbItems - 1){currentItem += 1;}
+            if ((kDown & KEY_UP) && currentItem > 0){currentItem -= 1;}
+
+            if (kDown & KEY_A){
+                // SET COLOR
+                currentScreen = 1;
+            }
+            //if (kDown & KEY_PLUS){
+                //BACKUP
+            //    currentScreen = 2;
+            //}
+            if (kDown & KEY_B)
+                break;
+        }
+        else if (currentScreen == 1){
+            std::cout << "Applying the " << colorProfiles[currentItem] << " color profile" << std::endl;
+            res = setColor(colorValues[currentItem]);
+            std::cout << "\n\n" << std::endl;
+            
+            if(res == 0){
+                std::cout << "Successfully changed the Joy-Cons color!" << std::endl;
+            }
+            if(res == 1 || res == 3){
+                std::cout << "Couldn't change the left JC color" << std::endl;
+            }
+            if(res == 2 || res == 3){
+                std::cout << "Couldn't change the right JC color" << std::endl;
+            }
+            if(res == 4){
+                std::cout << "Couldn't ID the joycons, make sure they're connected to the Switch" << std::endl;
+            }
+            std::cout << "Press [B] to return to the main menu" << std::endl;
+            if (kDown & KEY_B) currentScreen = 0;
+        }
+        else if(currentScreen == 2){
+            std::cout << "Press [B] to return to the main menu" << std::endl;
+        }
+
+
+
+        consoleUpdate(NULL);
+    }
+
+    hiddbgExit();
+	hidsysExit();
+    consoleExit(NULL);
+    return 0;
+}
